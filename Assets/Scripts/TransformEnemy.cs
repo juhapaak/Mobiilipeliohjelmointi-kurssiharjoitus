f7c78e4264d25044c7c20ffd8d100bbfaf13520f
Juha Pääkkönen <juhapaak@gmail.com>
2024-02-01 18:03:15 +0200

    Enemy basic movement
    Added a enemy basic movement with transform script

---
+++ Assets/Scripts/TransformEnemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformEnemy : MonoBehaviour
+{
+	private bool righty = true;
+	public float speed = 2.0f;
+	public float rightest = 1.0f;
+	public float leftest = -2.0f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	void Update()
+	{
+		if (righty)
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+		else
+			transform.Translate(-Vector2.right * speed * Time.deltaTime);
+
+		if (transform.position.x >= rightest)
+		{
+			righty = false;
+		}
+
+		if (transform.position.x <= leftest)
+		{
+			righty = true;
+		}
+	}
+}
