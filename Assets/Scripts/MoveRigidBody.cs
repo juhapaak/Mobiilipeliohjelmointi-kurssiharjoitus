86fd8923d5103336337eee9588302f15d0e52af2
Juha Pääkkönen <juhapaak@gmail.com>
2024-02-22 17:32:39 +0200

    Edited moverigid script
    Enemy homes to target, self destructs

--- Assets/Scripts/MoveRigidBody.cs
+++ Assets/Scripts/MoveRigidBody.cs
@@ -9,36 +9,33 @@ namespace harjoitus
     public class MoveRigidBody : MonoBehaviour
     {
         private Rigidbody2D sawRigidbody;
-        public Transform targer;
-        public float speed = 1f;
+        [SerializeField] private Transform target;
+        [SerializeField] public float speed = 1f;
 
         private void Awake()
         {
             sawRigidbody = GetComponent<Rigidbody2D>();
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         void FixedUpdate()
         {
             //Laskee suunnan
-            Vector2 direction = ((Vector2)targer.position - sawRigidbody.position).normalized;
+            Vector2 direction = ((Vector2)target.position - sawRigidbody.position).normalized;
 
             // Uusi sijainti nopeuden ja suunnan perusteella
             Vector2 newPosition = sawRigidbody.position + direction * speed * Time.fixedDeltaTime;
 
             // Liikuttaa olentoa
             sawRigidbody.MovePosition(newPosition);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Target"))
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
